9b5c93c868a4239d92d136d018f23c7b021f2b03
Gerardo Kohatus <gerardo.kohatus@gmail.com>
2026-01-08 18:50:28 -0300

    fix: url backend

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -26,7 +26,7 @@ if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
 # Set the SQLAlchemy URL from settings
-config.set_main_option("sqlalchemy.url", settings.DATABASE_URL)
+config.set_main_option("sqlalchemy.url", settings.DATABASE_URL.replace("%", "%%"))
 
 # add your model's MetaData object here for 'autogenerate' support
 target_metadata = Base.metadata
